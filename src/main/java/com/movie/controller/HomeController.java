b68463d42d2430c24126cef70d6e3e6c434e0451
Aditya <adityakumarthali@gmail.com>
2026-02-08 16:29:35 +0530

    add home controller

---
+++ src/main/java/com/movie/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.movie.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "✅ Your Movie REST API server is running!";
+    }
+}
